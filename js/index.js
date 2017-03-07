fc19c03bd04710ae1eb71745db1054c1bc68e8ee
Victor Cabrera <vcabre4@gmail.com>
2017-03-07 16:26:32 -0500

    a

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-(document).ready(() => {
+$(document).ready(() => {
   $('#sectTwo').on('click', '#startButton', () => {
+    alert('Hi');
     $('#startButton').css({ 'background-color': '#aa8f00',
       outline: 'none',
       'box-shadow': 'inset 0px 0px 4px #ccc' });
+    if (cookieUtil.readCookie('name')) {
+      window.location = 'quiz.html';
+    } else {
+      window.location = 'login.html';
+    }
   });
-  if (cookieUtil.readCookie('name')) {
-    window.location = 'quiz.html';
-    return;
-  }
-  window.location = 'login.html';
+});
+
+$(document).ready(() => {
+  $('#header').on('click', 'p', () => {
+    window.location = 'login.html';
+  });
 });
